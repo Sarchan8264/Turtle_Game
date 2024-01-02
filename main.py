55c6bab3f56abb175dace93e85a4cb9abfcb1e6f
Sarchan Azizov <serxanezizov085@gmail.com>
2024-01-02 13:35:38 +0400

    turtle

--- main.py
+++ main.py
@@ -1,16 +1,78 @@
-# This is a sample Python script.
+import turtle
+import time
+import random
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+delay = 0.1
+score = 0
+high_score = 0
+wn =turtle.Screen()
+wn.title("Snake Game")
+wn.bgcolor("blue")
+
+head=turtle.Turtle()
+head.shape("turtle")
+head.color("green")
+head.penup()
+head.goto(0,0)
+head.direction="Stop"
+
+food = turtle.Turtle()
+colors = random.choice(['red', 'orange', 'black'])
+shapes = random.choice(['square', 'triangle', 'circle'])
+food.speed(0)
+food.shape(shapes)
+food.color(colors)
+food.penup()
+food.goto(0, 100)
+
+pen = turtle.Turtle()
+pen.speed(0)
+pen.shape("square")
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 250)
+pen.write("Score : 0  High Score : 0", align="center",
+          font=("candara", 24, "bold"))
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def group():
+    if head.direction != "down":
+        head.direction = "up"
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def godown():
+    if head.direction != "up":
+        head.direction = "down"
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def goleft():
+    if head.direction != "right":
+        head.direction = "left"
+
+
+def goright():
+    if head.direction != "left":
+        head.direction = "right"
+
+
+def move():
+    if head.direction == "up":
+        y = head.ycor()
+        head.sety(y + 20)
+    if head.direction == "down":
+        y = head.ycor()
+        head.sety(y - 20)
+    if head.direction == "left":
+        x = head.xcor()
+        head.setx(x - 20)
+    if head.direction == "right":
+        x = head.xcor()
+        head.setx(x + 20)
+
+
+wn.listen()
+wn.onkeypress(group, "w")
+wn.onkeypress(godown, "s")
+wn.onkeypress(goleft, "a")
+wn.onkeypress(goright, "d")
